e005df411b9850aeaac462f1e2157e20c9314c4e
arvtom <tomkusarvydas@gmail.com>
2023-04-22 23:56:13 +0300

    change name to pcf8591. Call demo without building

--- demo.sh
+++ demo.sh
@@ -1,18 +1,17 @@
-make all
 sudo dmesg -C
 
 echo 'print kernel messages when hello_kernel is not yet inserted'
 sudo dmesg
 
-sudo insmod hello_kernel.ko
+sudo insmod pcf8591.ko
 
 echo 'print kernel messages when hello_kernel is inserted'
 sudo dmesg
 
 echo 'print active kernel modules containing keyword hello'
-lsmod | grep "hello"
+lsmod | grep "pcf8591"
 
-sudo rmmod hello_kernel
+sudo rmmod pcf8591
 
 echo 'print kernel messages when hello_kernel is removed'
 sudo dmesg
